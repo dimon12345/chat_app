f3a2f76257451f2a2da31a4794159af20a365afe
q q <q@q.q>
2024-08-19 23:10:49 +0700

    Activity added

---
+++ presentation/src/main/java/com/example/presentation/ChatActivity.kt
@@ -0,0 +1,17 @@
+package com.example.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class ChatActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Text("chat activity")
+        }
+    }
+}
